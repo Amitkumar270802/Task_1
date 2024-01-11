6ffb2222486a03f71a33c0e5271fa3e68a1c26ab
Amit Kumar <amitkumar270802@gmail.com>
2024-01-11 21:09:11 +0530

    Update App.js
    Add Animation

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./component/page/home/Home";
 import Footer from "./component/footer/Footer";
 import ExamDetails from "./component/page/examDetails/ExamDetails";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 function App() {
+  useEffect(() => {
+    AOS.init();
+  });
   return (
     <div className="App bg-slate-100 overflow-hidden">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />}  />
         <Route path="/exam" element={<ExamDetails />} />
       </Routes>
       <Footer />
