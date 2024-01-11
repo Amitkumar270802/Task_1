cb2552b36ee9303cc9f2269c22eab932f7f4622f
Amit Kumar <amitkumar270802@gmail.com>
2024-01-11 21:06:59 +0530

    Update ExamCard.jsx

--- src/component/page/home/ExamCard.jsx
+++ src/component/page/home/ExamCard.jsx
@@ -5,32 +5,40 @@ import { RiWallet3Fill } from "react-icons/ri";
 import { MdArrowForwardIos } from "react-icons/md";
 import { IoBookmark } from "react-icons/io5";
 import { Link } from "react-router-dom";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 const ExamCard = (props) => {
   const data = ["Engineering", "IIT", "PCM"];
   const [bookmark, setBookmark] = useState(1);
 
   let exams = props.data;
   return (
-    <div className="flex  mx-auto">
-      <div className="flex flex-wrap sm:justify-center gap-5  md:justify-center lg:justify-start items-center mx-auto m-4">
+    <div className="flex  mx-auto ">
+      <div className="flex justify-center gap-5 items-center mx-auto m-4">
         {exams &&
           exams.map((item, key) => (
             <div
-              className="shadow-slate-200 shadow-lg bg-white border border-gray-300 rounded-xl bg-smoke p-4 flex  flex-col h-[290px] w-[350px]  "
+              className="shadow-slate-200 shadow-lg bg-white border border-gray-300 rounded-xl bg-smoke p-4 flex  flex-col h-[300px] sm:w-[300px] w-[332px]  "
               key={key}
+              data-aos="flip-left"
+              data-aos-duration="1000"
             >
               <div className="flex items-center justify-between ">
                 <div className="flex justify-between items-center  p-1">
                   <div className="h-[40px] w-[40px] border border-gray-300 p-1 rounded-lg ">
                     <img src={item.image} />
                   </div>
-                  <div className="flex flex-col items-start p-2 ">
-                    <h4 className="font-bold text-xl">{item.name}</h4>
-                    <h6 className="text-gray-400 text-sm">{item.organiser}</h6>
+                  <div className="flex flex-col items-start justify-start p-2 ">
+                    <h4 className="font-bold text-lg">{item.name}</h4>
+                    <h6 className="text-gray-400 text-[14px]">
+                      {item.organiser}
+                    </h6>
                   </div>
                 </div>
                 <div
-                  className="text-2xl flex p-2  text-[#246bfd] cursor-pointer"
+                  className="text-xl flex   text-[#246bfd] cursor-pointer"
                   onClick={() => setBookmark(!bookmark)}
                 >
                   {bookmark ? <CiBookmark /> : <IoBookmark />}
@@ -63,7 +71,10 @@ const ExamCard = (props) => {
                       ))}
                   </div>
                   <div className="flex  mt-3 hover:cursor-pointer hover:scale-105">
-                    <Link to="/exam" className="text-[#246bfd]  ml-3 flex justify-center items-center">
+                    <Link
+                      to="/exam"
+                      className="text-[#246bfd]  ml-3 flex justify-center items-center"
+                    >
                       <div>View Details</div>
                       <div className="text-[14px] text-[#246bfd] font-extrabold p-2">
                         <MdArrowForwardIos />
